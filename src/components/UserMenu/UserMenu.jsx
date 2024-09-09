24392089efc71301b4c265521dcdcc13590095e7
Dovhan_Eduard <dovhan.eduard@gmail.com>
2024-09-09 18:35:37 +0300

    user loggedIn mark

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
+import { selectAuthUser } from '../../redux/auth/selectors';
+import css from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
+  const user = useSelector(selectAuthUser);
 
   const onLogout = () => {
     dispatch(logout());
   };
   return (
-    <div>
+    <div className={css.userMenu}>
+      <h3>{user.name}</h3>
       <button type="button" onClick={onLogout}>
         Вийти
       </button>
